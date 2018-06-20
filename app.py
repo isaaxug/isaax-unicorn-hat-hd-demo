f0aed2d11a4a132614101adee20d77e09af76dc6
Atsushi <atsushi@moongift.jp>
2018-06-20 15:55:10 +0900

    Ignore error

--- app.py
+++ app.py
@@ -13,45 +13,45 @@ except ImportError:
 
 import unicornhathd
 
-lines = [os.environ.get('MESSAGE')]
+def show():
+    lines = os.environ.get('MESSAGE').split(',')
 
-colours = [tuple([int(n * 255) for n in colorsys.hsv_to_rgb(x/float(len(lines)), 1.0, 1.0)]) for x in range(len(lines))]
+    colours = [tuple([int(n * 255) for n in colorsys.hsv_to_rgb(x/float(len(lines)), 1.0, 1.0)]) for x in range(len(lines))]
 
-FONT = ("/usr/share/fonts/truetype/roboto/Roboto-Bold.ttf", 10)
+    FONT = ("/usr/share/fonts/truetype/roboto/Roboto-Bold.ttf", 10)
 
-unicornhathd.rotation(0)
-unicornhathd.brightness(1.0)
+    unicornhathd.rotation(0)
+    unicornhathd.brightness(1.0)
 
-width, height = unicornhathd.get_shape()
+    width, height = unicornhathd.get_shape()
 
-text_x = width
-text_y = 2
+    text_x = width
+    text_y = 2
 
 
-font_file, font_size = FONT
+    font_file, font_size = FONT
 
-font = ImageFont.truetype(font_file, font_size)
+    font = ImageFont.truetype(font_file, font_size)
 
-text_width, text_height = width, 0
+    text_width, text_height = width, 0
 
-for line in lines:
-    w, h = font.getsize(line)
-    text_width += w + width
-    text_height = max(text_height,h)
+    for line in lines:
+        w, h = font.getsize(line)
+        text_width += w + width
+        text_height = max(text_height,h)
 
-text_width += width + text_x + 1
+    text_width += width + text_x + 1
 
-image = Image.new("RGB", (text_width,max(16, text_height)), (0,0,0))
-draw = ImageDraw.Draw(image)
+    image = Image.new("RGB", (text_width,max(16, text_height)), (0,0,0))
+    draw = ImageDraw.Draw(image)
 
-offset_left = 0
+    offset_left = 0
 
-for index, line in enumerate(lines):
-    draw.text((text_x + offset_left, text_y), line, colours[index], font=font)
+    for index, line in enumerate(lines):
+        draw.text((text_x + offset_left, text_y), line, colours[index], font=font)
 
-    offset_left += font.getsize(line)[0] + width
+        offset_left += font.getsize(line)[0] + width
 
-def show(unicornhathd, image, text_width, width, height):
     for scroll in range(text_width - width):
         for x in range(width):
             for y in range(height):
@@ -66,5 +66,5 @@ def show(unicornhathd, image, text_width, width, height):
     return True
 
 while True:
-    show(unicornhathd, image, text_width, width, height)
+    show()
     time.sleep(3)
